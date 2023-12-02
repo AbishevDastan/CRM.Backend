19491e0487781cfe91072ecbef9d2656b5f0db6f
Dastan Abishev <abish.ev@mail.ru>
2023-12-02 22:12:56 +0100

    Web API controller methods for TaskItem entity implemented.

---
+++ Web/Controllers/TaskItemController.cs
@@ -0,0 +1,75 @@
+﻿using Application.Services.TaskItemService;
+using Application.UseCases.Employee;
+using Application.UseCases.TaskItem;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemController : ControllerBase
+    {
+        private readonly ITaskItemService _taskItemService;
+
+        public TaskItemController(ITaskItemService taskItemService)
+        {
+            _taskItemService = taskItemService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TaskItemDto>>> GetTaskItems()
+        {
+            var taskItems = await _taskItemService.GetTaskItems();
+
+            if (taskItems == null)
+                return NotFound();
+
+            return Ok(taskItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskItemDto>> GetTaskItem(int id)
+        {
+            var taskItem = await _taskItemService.GetTaskItem(id);
+
+            if (taskItem == null)
+                return NotFound();
+
+            return Ok(taskItem);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskItemDto>> AddTaskItem(TaskItemDto taskItemDto)
+        {
+            var result = await _taskItemService.AddTaskItem(taskItemDto);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<TaskItemDto>> UpdateTaskItem(TaskItemDto taskItemDto)
+        {
+            var result = await _taskItemService.UpdateTaskItem(taskItemDto);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteTaskItem(int id)
+        {
+            var result = await _taskItemService.DeleteTaskItem(id);
+
+            if (result != true)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
