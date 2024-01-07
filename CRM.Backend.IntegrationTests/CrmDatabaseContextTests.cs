e1c48d049deafa403acb636a4797717ebfde373d
Dastan Abishev <abish.ev@outlook.com>
2024-01-07 02:14:52 -0800

    First simple integration tests implemented.

---
+++ CRM.Backend.IntegrationTests/CrmDatabaseContextTests.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence.DatabaseContext;
+using Shouldly;
+
+namespace CRM.Backend.IntegrationTests
+{
+    public class CrmDatabaseContextTests
+    {
+        private CrmDatabaseContext _crmDatabaseContext;
+
+        public CrmDatabaseContextTests()
+        {
+            var dbOptions = new DbContextOptionsBuilder<CrmDatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            _crmDatabaseContext = new CrmDatabaseContext(dbOptions);
+        }
+
+        [Fact]
+        public async void SetDateCreatedValue()
+        {
+            var employee = new Employee { Id = 1, FullName = "Dastan Abishev", Position = "Senior Full Stack Developer" };
+
+            await _crmDatabaseContext.Employees.AddAsync(employee);
+            await _crmDatabaseContext.SaveChangesAsync();
+
+            employee.DateCreated.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async void SetDateModifiedValue()
+        {
+            var employee = new Employee { Id = 1, FullName = "Dastan Abishev", Position = "Senior Full Stack Developer" };
+
+            await _crmDatabaseContext.Employees.AddAsync(employee);
+            await _crmDatabaseContext.SaveChangesAsync();
+
+            employee.DateModified.ShouldNotBeNull();
+        }
+    }
+}
---
+++ CRM.Backend.UnitTests/Features/Employees/Queries/GetEmployeeDetailsQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.Contracts.Persistence;
+using Application.MappingProfiles;
+using AutoMapper;
+using CRM.Backend.UnitTests.Mocks;
+using Moq;
+
+namespace CRM.Backend.UnitTests.Features.Employees.Queries
+{
+    public class GetEmployeeDetailsQueryHandlerTests
+    {
+        private readonly Mock<IEmployeeRepository> _mockRepo;
+        private IMapper _mapper;
+
+        public GetEmployeeDetailsQueryHandlerTests()
+        {
+            _mockRepo = MockEmployeeRepository.GetMockEmployeeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<EmployeeProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetEmployeeDetailsTest()
+        {
+
+        }
+    }
+}
