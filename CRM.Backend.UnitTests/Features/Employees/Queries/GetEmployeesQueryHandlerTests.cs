659348bea89fcbde57204efcbd5bdffe49688315
Dastan Abishev <abish.ev@outlook.com>
2024-01-07 01:55:53 -0800

    Unit test for GetEmployeesQueryHandler implemented.

---
+++ CRM.Backend.UnitTests/Features/Employees/Queries/GetEmployeesQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Application.Contracts.Logging;
+using Application.Contracts.Persistence;
+using Application.Features.Employee.Queries.GetAllEmployees;
+using Application.Features.Employee.Shared;
+using Application.MappingProfiles;
+using AutoMapper;
+using CRM.Backend.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace CRM.Backend.UnitTests.Features.Employees.Queries
+{
+    public class GetEmployeesQueryHandlerTests
+    {
+        private readonly Mock<IEmployeeRepository> _mockRepo;
+        private IMapper _mapper;
+
+        public GetEmployeesQueryHandlerTests()
+        {
+            _mockRepo = MockEmployeeRepository.GetMockEmployeeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<EmployeeProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetEmployeesTest()
+        {
+            var handler = new GetEmployeesQueryHandler(_mapper, _mockRepo.Object);
+
+            var result = await handler.Handle(new GetEmployeesQuery(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<EmployeeDto>>();
+            result.Count.ShouldBe(5);
+        }
+    }
+}
---
+++ CRM.Backend.UnitTests/Mocks/MockEmployeeRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts.Persistence;
+using Domain;
+using Moq;
+
+namespace CRM.Backend.UnitTests.Mocks
+{
+    public class MockEmployeeRepository
+    {
+        public static Mock<IEmployeeRepository> GetMockEmployeeRepository()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { Id = 1, FullName = "Dastan Abishev", Position = "Senior Full Stack Developer" },
+                new Employee { Id = 2, FullName = "Diana Levchenko", Position = "Cloud & Infra Management Engineer" },
+                new Employee { Id = 3, FullName = "Damir Abishev", Position = "Musician" },
+                new Employee { Id = 4, FullName = "Zhansaya Abishev", Position = "Musician" },
+                new Employee { Id = 5, FullName = "Adele Abisheva", Position = "Srygul'" },
+            };
+
+            var mockRepo = new Mock<IEmployeeRepository>();
+
+            mockRepo.Setup(r => r.GetAll()).ReturnsAsync(employees);
+            mockRepo.Setup(r => r.Create(It.IsAny<Employee>()))
+                .Returns((Employee employee) =>
+                {
+                    employees.Add(employee);
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
+    }
+}
